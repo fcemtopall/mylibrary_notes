c39a50d9756e73abad6fc9220e63738a43da5831
fcemtopall <f.c.topal@gmail.com>
2024-06-08 22:28:44 +0300

    development : creating user table and models

---
+++ src/main/kotlin/com/mylibrarynotes/repository/Repository.kt
@@ -0,0 +1,36 @@
+package com.mylibrarynotes.repository
+
+import com.mylibrarynotes.data.model.User
+import com.mylibrarynotes.data.table.UserTable
+import com.mylibrarynotes.repository.DatabaseFactory.dbQuery
+import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.transactions.transaction
+
+class Repository {
+
+    suspend fun addUser(user: User){
+        dbQuery{
+            UserTable.insert{ userTable ->
+                userTable[UserTable.email] = user.email
+                userTable[UserTable.password] = user.password
+                userTable[UserTable.hashedPassword] = user.hashedPassword
+                userTable[UserTable.name] = user.username
+            }
+        }
+    }
+
+    suspend fun findUserByEmail(email: String) = dbQuery {
+        UserTable.select(UserTable.email.eq(email))
+            .map { rowToUser(it) }
+            .singleOrNull()
+    }
+
+    private fun rowToUser(row: ResultRow): User? {
+        return User(email = row[UserTable.email],
+            password = row[UserTable.password],
+            hashedPassword = row[UserTable.hashedPassword],
+            username = row[UserTable.name])
+    }
+}
